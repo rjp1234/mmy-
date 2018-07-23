68f9450ed91ca4cdc94e47e1e461f4c47fd65b89
renjianpingHome <690876723@qq.com>
2018-07-23 22:42:36 +0800

    “应对应对yi信应对微信审核的新设计

---
+++ sysuser.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 123.207.146.145
+ Source Server Type    : MySQL
+ Source Server Version : 50722
+ Source Host           : 123.207.146.145:3306
+ Source Schema         : mengmengyuan
+
+ Target Server Type    : MySQL
+ Target Server Version : 50722
+ File Encoding         : 65001
+
+ Date: 23/07/2018 20:55:31
+*/
+
